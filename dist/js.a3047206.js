1a0948a15ed4e8fa7a4a2a88d03c7617849e93a5
kaspertidemann <kt@tdmn.co>
2018-07-24 12:14:52 +0200

    Set the correct date of the Alefarm post.

--- dist/js.a3047206.js
+++ dist/js.a3047206.js
@@ -1442,7 +1442,7 @@ var journalEntries = exports.journalEntries = [{
     lastname: 'Tidemann'
   },
   content: 'Greve, Denmark – We are tremendously happy and proud to announce the opening of the brand new Alefarm Brewing.\n\nWe have been on a journey that took nearly a year from finding the right location to getting all equipment installed and running smoothly. It has been quite the ride - fun, challenging and deeply rewarding.\n\nAt the new brewery, we will have a fixed line-up of beers supplemented by seasonal offerings, new releases and collaborations. The production is carried forward by an explorative, creative and spontaneous brewing philosophy.\n\nWe are delighted to welcome two new brewers and a brand manager to the Alefarm family. Every single person at Alefarm Brewing is a part of our common story and we are ecstatic to have such talented people join us.\n\nI would like to say a profound thank you to everyone involved in the process. Thank you for helping out, for believing in us and for lending an ear when times were rough.\n\nNow, good times await and excellent beers are to be made. We are so excited and look forward to welcoming you to the new brewery. Cheers!',
-  created: new Date().toISOString(),
+  created: '2018-07-24T10:13:23.120Z',
   excerpt: 'We are tremendously happy and proud to announce the opening of the brand new Alefarm Brewing at the new location in Greve, just outside Copenhagen.',
   id: '1',
   slug: 'proudly-presenting-alefarm-brewing-2-0',
